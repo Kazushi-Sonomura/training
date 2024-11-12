b8e4b714d865a1accd2d577b31a995ece22ec47a
Kazushi-Sonomura <KAZUSHI.SONOMURA@grp.tmnf.jp>
2024-11-12 23:56:46 +0900

    23:56

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <NuxtPage />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
-
+<script setup>
+  useHead(
+    {
+      title: "Nuxt 3 Store"
+    }
+  )
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <dev class="container">
+        <slot />
+        <footer> &copy; proglus.jp</footer>
+    </dev>
+</template>
+<style>
+.container {
+    color: red;
+}
+footer {
+    background-color: orange;
+    color: white;
+    text-align: center;
+}
+</style>
